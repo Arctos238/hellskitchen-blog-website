82f76dd2552d0374ee7ac3ce930bb3b641498f03
Jacque Pointer <93753375+Arctos238@users.noreply.github.com>
2023-01-30 17:03:29 -0700

    Finished V1.0

---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import { FeaturedPosts } from "../sections/index";
+import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
+
+export default function Home({ posts }) {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      {/* <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IM+Fell+English:ital@1&display=swap"
+          rel="stylesheet"
+        />
+      </Head> */}
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
